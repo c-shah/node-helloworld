3422ba7f6eeff06ccfba2092a3b0ed10a5d960c5
c-shah <chintanjshah@gmail.com>
2017-06-12 19:42:51 -0700

    changes

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 /**
  * Created by rr245546 on 6/12/2017.
  */
+console.log(' am i here ?? ');
+var http = require('http');
+var port = process.env.port || 3000;
+console.log(' port ' + port );
+
+
+http.createServer(onRequest).listen( port );
+
+function onRequest(request, response) {
+    console.log(' on request ');
+    response.writeHead(200);
+    response.write('Chintan Shah');
+    response.end();
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+#### create prof file
+    web: node app.js
+
+#### Running Locally
+    1. heroku local web
+    2. Intellij Run (ctrl+shft+a)
+    3. nodemon app.js
+
+#### Env file 
+- It is only used when we run heroku locally (heroku local web)
+- It is not used when running from server
